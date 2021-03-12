31bf20febd2d16782770f96b6553d3ae93bda19a
ting-yummy <Jiangdt960225@163.com>
2021-03-12 14:17:16 +0800

    两数之和

--- algorithm/src/test/Solution.java
+++ algorithm/src/test/Solution.java
@@ -1,8 +1,61 @@
 package test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author jdt
  * @date 2021/3/12 13:54
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * 你可以按任意顺序返回答案。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ *
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ *
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ *  
+ *
+ * 提示：
+ *
+ * 2 <= nums.length <= 103
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * 只会存在一个有效答案
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 13;
+        int[] ints = twoSum(nums, target);
+        System.out.println(Arrays.toString(ints));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        //key是值，value是下标
+        Map<Integer,Integer> result = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+           int num = target-nums[i];
+           if(result.containsKey(num)){
+               return new int[]{result.get(num),i};
+           }else {
+               result.put(nums[i],i);
+           }
+
+        }
+        return null;
+    }
 }
